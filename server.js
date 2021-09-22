9289c87cbce0ff2bdf61b0466267d52d2053f7c1
shahrier82 <shahrierf82@gmail.com>
2021-09-22 23:16:43 +0600

    ready to deploy v2

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
