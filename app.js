7236472b5398ca370cd27e8c56e6f371361d8dd9
shahrier82 <shahrierf82@gmail.com>
2021-09-29 21:08:57 +0600

    ready to deploy

--- app.js
+++ app.js
@@ -20,19 +20,13 @@ app.use(cors());
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('frontend/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello from the server!',
-  });
-});
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   });
+// }
 
 app.use('/api/exercise', exerciseRouter);
 app.use('/api/users', userRouter);
