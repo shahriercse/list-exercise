48382478623562f0896272fa6748719aa42c60f1
shahrier82 <shahrierf82@gmail.com>
2021-09-30 21:00:14 +0600

    changes app

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.use('/api/exercise', exerciseRouter);
 app.use('/api/users', userRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
