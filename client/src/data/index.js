43bf887e361d797e026f30e83914373b8d69f7e9
shahrier82 <shahrierf82@gmail.com>
2021-09-30 21:13:48 +0600

    changes client

--- client/src/data/index.js
+++ client/src/data/index.js
@@ -1,57 +1,49 @@
 import axios from 'axios';
 
 export async function getExercises() {
-  const res = await axios.get('http://localhost:8206/api/exercise');
+  const res = await axios.get('/api/exercise');
 
   return res.data.exercises;
 }
 
 export async function getExercise(id) {
-  const res = await axios.get(`http://localhost:8206/api/exercise/${id}`);
+  const res = await axios.get(`/api/exercise/${id}`);
 
   return res.data;
 }
 
 export async function deleteExercise(id) {
-  const res = await axios.delete(`http://localhost:8206/api/exercise/${id}`);
+  const res = await axios.delete(`/api/exercise/${id}`);
 
   return res.data;
 }
 
 export async function getUsers() {
-  const res = await axios.get('http://localhost:8206/api/users');
+  const res = await axios.get('/api/users');
 
   return res.data;
 }
 
 export async function createExercise(inputs) {
-  const res = await axios.post(
-    'http://localhost:8206/api/exercise/add',
-    inputs
-  );
+  const res = await axios.post('/api/exercise/add', inputs);
 
   return res.data;
 }
 
 export async function updateExercise(id, inputs) {
-  const res = await axios.patch(
-    `http://localhost:8206/api/exercise/${id}`,
-    inputs
-  );
+  const res = await axios.patch(`/api/exercise/${id}`, inputs);
 
   return res.data;
 }
 
 export async function createUser(inputs) {
-  const res = await axios.post('http://localhost:8206/api/users/add', inputs);
+  const res = await axios.post('/api/users/add', inputs);
 
   return res.data;
 }
 
 export async function archiveExercise(id) {
-  const res = await axios.patch(
-    `http://localhost:8206/api/exercise/archive/${id}`
-  );
+  const res = await axios.patch(`/api/exercise/archive/${id}`);
 
   return res.data;
 }
