c899a793f54816f7a1522bdd92d0c1536573853b
shahrier82 <shahrierf82@gmail.com>
2021-09-30 19:47:52 +0600

    heroku setup

--- app.js
+++ app.js
@@ -20,16 +20,22 @@ app.use(cors());
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true }));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('frontend/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 app.use('/api/exercise', exerciseRouter);
 app.use('/api/users', userRouter);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello from the server!',
+  });
+});
 
 // SHOWING AN ERROR - IF REQUEST FROM UNKNOWN ROUTES
 app.all('*', (req, res, next) => {
